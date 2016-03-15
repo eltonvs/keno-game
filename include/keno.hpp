6e17752cdeda94e9151d5497ac4ba4713389c8dc
Elton Viana <eltonvs@outlook.com>
2016-03-15 09:56:47 -0300

    Added initial files

---
+++ include/keno.hpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2016 - Elton Viana
+ */
+
+#ifndef _keno_hpp_
+#define _keno_hpp_
+
+// Code Here
+
+#endif
---
+++ src/keno.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2016 - Elton Viana
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    // Code Here
+
+    return EXIT_SUCCESS;
+}
