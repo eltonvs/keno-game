f386651722dbad81294cedc6db03cd9cc1e094fa
Elton Viana <eltonvs@outlook.com>
2016-03-19 22:01:20 -0300

    Receive data from cli

--- src/drive_keno.cpp
+++ src/drive_keno.cpp
@@ -1,11 +1,34 @@
 // Copyright 2016
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include "keno.hpp"
 
+constexpr auto MAX_SPOTS(15u);
+constexpr auto MAX_PROPER(80u);
+
 int main(int argc, char const *argv[]) {
-    // Code Here
+    // Store the bet file name
+    std::string bet_file_name = "default_bet.dat";
+
+    // Verify args passed by cli
+    if (argc == 1)
+        std::cerr << "No file specified, running with the default bet file\n";
+    else
+        bet_file_name = argv[1];
+
+    // Open bet file
+    std::ifstream bet("data/"+bet_file_name);
+
+    // Verify if the bet file was opened
+    if (bet)
+        std::cout << "File opened with success" << std::endl;
+    else
+        std::cerr << "Error at file opening" << std::endl;
+
+    bet.close();
 
     return EXIT_SUCCESS;
 }
